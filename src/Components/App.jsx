d3a515434f7666e5f69da7f5fec6cabb2aa28792
Wise-Wizard <saransh.shankar@gmail.com>
2023-01-17 23:19:11 +0530

    Added Notes file

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import Footer from './Footer';
 import Heading from './Header';
 import Note from './Note';
+import notes from '../notes.js';
+
+function createNote(noteDesc){
+    return <Note
+        key={noteDesc.id}
+        title={noteDesc.title}
+        content={noteDesc.content}
+    />
+}
 
 function App(){
     return <div>
     <Heading />
     <Footer />
+    {notes.map(createNote)}
     <Note />
     </div>
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return <footer>
         <p>
-            Copyright © 2023
+            Copyright © 2023  
         </p>
     </footer>
 }
